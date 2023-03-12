655a9190c90102bacd9ae967b182ec957d16dfd1
Николай <raspaev.niko@mail.ru>
2023-03-12 11:51:39 +0400

    fix

--- FoodOrders/FoodOrdersContracts/ViewModels/DishViewModel.cs
+++ FoodOrders/FoodOrdersContracts/ViewModels/DishViewModel.cs
@@ -6,7 +6,7 @@ namespace FoodOrdersContracts.ViewModels
     public class DishViewModel : IDishModel
     {
         public int Id { get; set; }
-        [DisplayName("Название изделия")]
+        [DisplayName("Название компонента")]
         public string DishName { get; set; } = string.Empty;
         [DisplayName("Цена")]
         public double Price { get; set; }
